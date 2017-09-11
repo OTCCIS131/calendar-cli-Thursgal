b92a61bf0de3ac0f9f5a3530b57edac6c354505e
Unknown <bo0842372@otc.edu>
2017-09-11 13:26:40 -0500

    Made days appear correct(ish)

--- index.js
+++ index.js
@@ -5,14 +5,50 @@ const MomentRange = require('moment-range');
  
 const moment = MomentRange.extendMoment(Moment);
 
-const date = moment();
-const range = date.range('year');
-let dow=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var dayValue;
+var daysArray = [];
+var date = moment();
+var range = date.range('year');
+let dow=['   S ', 'M ', 'T ', 'W ', 'Th', 'F', 'S'];
 
 for (let month of range.by('month')) {
-  console.log(month.format('MMMM dddd'));
-    for (let day of range.by('day')) {
-        console.log(day.format('dd'));
+  console.log(month.format('MMMM'));
+  switch(month.format('dddd')) {
+    case 'Sunday':
+        break;
+    case 'Monday':
+        for (i = 0; i < 1; i++)
+            daysArray.push('  ');
+        break;
+    case 'Tuesday':
+        for (i = 0; i < 2; i++)
+            daysArray.push('  ');
+        break;
+    case 'Wednesday':
+        for (i = 0; i < 3; i++)
+            daysArray.push('  ');
+        break;
+    case 'Thursday':
+        for (i = 0; i < 4; i++)
+            daysArray.push('  ');
+        break;
+    case 'Friday':
+        for (i = 0; i < 5; i++)
+            daysArray.push('  ');
+        break;
+    case 'Saturday':
+        for (i = 0; i < 6; i++)
+            daysArray.push('  ');
+        break;
+    default:
+        break;
     }
-    console.log('END OF MONTH');
+  var nRange = date.range('month');
+    for (let day of nRange.by('day')) {
+        daysArray.push(day.format('DD'));
+    }
+    displayArray = _.chunk(daysArray, 7);
+    console.log(dow);
+    console.log(displayArray);
+    daysArray = [];
 }
