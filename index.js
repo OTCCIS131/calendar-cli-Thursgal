2a7439a92d5a5ad2e400f6f355abee442ef6b62f
Unknown <bo0842372@otc.edu>
2017-09-13 13:37:13 -0500

    Fixed display days

--- index.js
+++ index.js
@@ -1,54 +1,47 @@
-const _ = require('lodash');
-const chalk = require('chalk');
-const Moment = require('moment');
-const MomentRange = require('moment-range');
+var _ = require('lodash');
+var chalk = require('chalk');
+var Moment = require('moment');
+var MomentRange = require('moment-range');
  
-const moment = MomentRange.extendMoment(Moment);
+var moment = MomentRange.extendMoment(Moment);
 
 var dayValue;
+
+
+var display;
+var paddedDays = [];
 var daysArray = [];
 var date = moment();
 var range = date.range('year');
-let dow=['   S ', 'M ', 'T ', 'W ', 'Th', 'F', 'S'];
+
+let dow=['S', 'M', 'T', 'W', 'Th', 'F', 'S'];
 
 for (let month of range.by('month')) {
-  console.log(month.format('MMMM'));
-  switch(month.format('dddd')) {
-    case 'Sunday':
-        break;
-    case 'Monday':
-        for (i = 0; i < 1; i++)
-            daysArray.push('  ');
-        break;
-    case 'Tuesday':
-        for (i = 0; i < 2; i++)
-            daysArray.push('  ');
-        break;
-    case 'Wednesday':
-        for (i = 0; i < 3; i++)
-            daysArray.push('  ');
-        break;
-    case 'Thursday':
-        for (i = 0; i < 4; i++)
-            daysArray.push('  ');
-        break;
-    case 'Friday':
-        for (i = 0; i < 5; i++)
-            daysArray.push('  ');
-        break;
-    case 'Saturday':
-        for (i = 0; i < 6; i++)
-            daysArray.push('  ');
-        break;
-    default:
-        break;
-    }
-  var nRange = date.range('month');
-    for (let day of nRange.by('day')) {
-        daysArray.push(day.format('DD'));
-    }
-    displayArray = _.chunk(daysArray, 7);
-    console.log(dow);
-    console.log(displayArray);
+    console.log(_.pad(month.format('MMMM'), 26, '-'));
+
+
+    daysArray = Array.from(month.range('month').by('days'));
+    paddedDays = _.map(daysArray, day => {
+        date = day.date();
+        if (day.month() == 8 && day.date() == 10) {
+            date = chalk.red(date);
+        }
+        else if (day.month() == 9 && day.date() == 30) {
+            date = chalk.blue(date);
+        }
+        return _.padEnd(date, 2, ' ');
+    })
+
+    switch(month.day()) {
+        
+    } // End of switch
+
+    console.log("S   M   T   W   Th  F   S");
+    paddedDays = _.chunk(paddedDays, 7);
+    paddedDays.forEach(week => {console.log(week.join("  "))})
+
+
+    
+    console.log();
     daysArray = [];
 }
